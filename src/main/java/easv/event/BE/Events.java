daf3784c4047e204ccce78203a7c8a7fc0770b65
thorjessing <thorwinddk@gmail.com>
2025-03-16 22:29:54 +0100

    Tilføjet BE - Events,Tickets & Users

---
+++ src/main/java/easv/event/BE/Events.java
@@ -0,0 +1,89 @@
+package easv.event.BE;
+
+import java.time.LocalDate;
+
+public class Events {
+    private int id;
+    private String title;
+    private String description;
+    private LocalDate date;
+    private String startsAt;
+    private String location;
+
+    public Events(int id, String title, String description, LocalDate date, String startsAt, String location) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.startsAt = startsAt;
+        this.location = location;
+    }
+
+    public Events(String location, String startsAt, LocalDate date, String description, String title) {
+        this.location = location;
+        this.startsAt = startsAt;
+        this.date = date;
+        this.description = description;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getStartsAt() {
+        return startsAt;
+    }
+
+    public void setStartsAt(String startsAt) {
+        this.startsAt = startsAt;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Events{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", date='" + date + '\'' +
+                ", startsAt='" + startsAt + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/easv/event/BE/Tickets.java
@@ -0,0 +1,51 @@
+package easv.event.BE;
+
+public class Tickets {
+    private int id;
+    private String name;
+    private int type;
+
+    public Tickets(int id, String name, int type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public Tickets(int type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Tickets{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type=" + type +
+                '}';
+    }
+}
